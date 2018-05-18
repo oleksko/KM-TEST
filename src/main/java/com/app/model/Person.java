3624e4f1b24f724f12bc07fe4863e066766759e8
KONRAD <konrad@gmail.com>
2018-05-18 17:45:11 +0100

    MODEL AGAIN

--- src/main/java/com/app/model/Person.java
+++ src/main/java/com/app/model/Person.java
@@ -1,37 +1,6 @@
 package com.app.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Person {
     private Long id;
     private String name;
-
-    public Person() {
-    }
-
-    public Person(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public static List<Person> generatePeople() {
-        return new ArrayList<>();
-    }
 }
